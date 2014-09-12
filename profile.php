f42acc351d8fb80e6f78104d5b371f27ebe9c6c7
Matt Thomas <matt@betweenbrain.com>
2014-09-12 08:14:09 -0400

    Initial import of Joomla user profile plugin with a couple of fixes

---
+++ profile.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  User.profile
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+/**
+ * An example custom profile plugin.
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  User.profile
+ * @since       1.6
+ */
+class PlgUserProfile extends JPlugin
+{
+	/**
+	 * Date of birth.
+	 *
+	 * @var    string
+	 * @since  3.1
+	 */
+	private $date = '';
+
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * adds additional fields to the user editing form
+	 *
+	 * @param   JForm $form The form to be altered.
+	 * @param   array $data The associated data for the form.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   1.6
+	 */
+	public function onContentPrepareForm($form, $data)
+	{
+		if (!($form instanceof JForm))
+		{
+			$this->_subject->setError('JERROR_NOT_A_FORM');
+
+			return false;
+		}
+
+		// Add the registration fields to the form.
+		JForm::addFormPath(__DIR__ . '/profiles');
+
+		$form->loadFile('profile', false);
+
+		return true;
+	}
+}
